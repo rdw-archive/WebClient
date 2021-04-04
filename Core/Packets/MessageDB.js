0e22d8eaa40fc2a4b80d6d682334f0bcaec8a4a2
RDW <duckwhale@mail.ru>
2021-04-04 02:10:58 +0200

    Core: Added prototype of the packet definitions
    Needs streamlining, but that's a separate issue.

---
+++ Core/Packets/MessageDB.js
@@ -0,0 +1,53 @@
+class Message {
+	constructor() {
+		this.event = "PING";
+		this.version = 0;
+		this.payload = {};
+	}
+	toString() {
+		const messageString =
+			this.event +
+			MESSAGE_TOKEN_SEPARATOR +
+			this.version +
+			MESSAGE_TOKEN_SEPARATOR +
+			JSON.stringify(this.payload);
+		return messageString;
+	}
+}
+
+class ConnectionEstablishedMessage extends Message {
+	constructor(event, version, payload) {
+		super();
+
+		this.event = event;
+		this.version = version;
+		this.payload = {
+			accountID: payload.shift(),
+		};
+	}
+}
+
+class MacroUpdateRequest extends Message {
+	constructor(event, version, payload) {
+		super();
+
+		this.event = event;
+		this.version = version;
+
+		if (version === 1)
+			this.payload = {
+				macroID: payload.shift(),
+				macroText: payload.shift(),
+			};
+		console.log(this);
+	}
+}
+
+const GLOBAL_MESSAGE_DB = {
+	// WorldServer -> Client
+	WEBSOCKET_CONNECTION: ConnectionEstablishedMessage,
+	PONG: Message,
+	// Client -> WorldServer
+	REQUEST_MACRO_UPDATE: MacroUpdateRequest,
+	PING: Message, // doesn't need to send any information
+};
