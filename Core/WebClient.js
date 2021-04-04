efab1c1875e820971afe8249ae54263cde575b73
RDW <duckwhale@mail.ru>
2021-04-04 19:11:33 +0200

    Core: Fixed relative path lookups in binary releases

--- Core/WebClient.js
+++ Core/WebClient.js
@@ -57,7 +57,7 @@ class WebClient {
 		document.head.appendChild(link);
 	}
 	static loadManifest() {
-		const filePath = "TOC.json";
+		const filePath = WEBCLIENT_ROOT_DIR + "/TOC.json";
 		DEBUG("Loading application manifest from " + filePath);
 		this.metadata = C_FileSystem.readJSON(filePath);
 	}
@@ -86,7 +86,7 @@ class WebClient {
 	static onScriptExecutionFinished(event, URL) {
 		// Kinda ugly, but alas. It's better than entering callback hell and sync loading doesn't work at all for this
 		DEBUG(format("SCRIPT_EXECUTION_FINISHED triggered for URL %s", URL));
-		if (URL !== "./Interface/Frames/WorldFrame.js") return;
+		if (URL !== WEBCLIENT_INTERFACE_DIR + "/Frames/WorldFrame.js") return;
 
 		// process.on("exit", function () {
 		window.onbeforeunload = function () {
