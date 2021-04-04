c6df3a7e6c33ec22b5be4e81f059a6cf463037e7
RDW <duckwhale@mail.ru>
2021-04-04 03:07:15 +0200

    Init: Added functionality to start the client

--- init.js
+++ init.js
@@ -1,2 +1,23 @@
+// Shorthand because I'm lazy (must be set after the localization tables have been read)
+let L = {};
 function StartWebClient() {
+	C_Profiling.startTimer("StartWebClient");
+
+	C_Logging.createLoggers();
+
+	WebClient.initializeLocalizationTables();
+	L = C_Locales.getLocalizationTable(WEBCLIENT_ACTIVE_LOCALE);
+	WebClient.setWindowTitle(L["Loading..."]);
+
+	C_Macro.restoreMacroCache(); // Needs to be done before addons are loaded, as they may want to interact with the cache?
+
+	WebClient.loadManifest();
+	WebClient.createUserInterface();
+	WebClient.loadEnabledAddons();
+
+	const TOC = WebClient.metadata;
+	const windowTitle = format("%s (%s)", TOC.titleString, TOC.versionString);
+	WebClient.setWindowTitle(windowTitle);
+
+	C_EventSystem.registerEvent("SCRIPT_EXECUTION_FINISHED", "WebClient", WebClient.onScriptExecutionFinished);
 }
