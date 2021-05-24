9d3dccc9d7eaddae277c4a91732be69ebd57ef44
RDW <duckwhale@mail.ru>
2021-05-24 22:40:19 +0200

    Core: Added AddOn class to load addons more conveniently
    Inspired by AceAddon; using this class' constructor will register the onLoad method automatically and therefore allows the addon author to skip writing some useless boilerplate code.
    
    The old method of manually registering for the ADDON_LOADED event obviously still works, so this is yet another optional shorthand that shouldn't cause any compatibility issues.

---
+++ Core/Classes/AddOn.js
@@ -0,0 +1,22 @@
+class AddOn {
+	constructor(addonName, isLoadOnDemand = false) {
+		this.name = addonName;
+		this.isLoadOnDemand = isLoadOnDemand;
+
+		if (isLoadOnDemand) return; // Deferred loading means we don't need to do anything else here
+
+		const self = this;
+		C_EventSystem.registerEvent("ADDON_LOADED", addonName, function (event, name) {
+			self.onAddonLoaded(event, name);
+		});
+	}
+	onAddonLoaded(event, addonName) {
+		if (addonName !== this.name) return;
+
+		C_EventSystem.unregisterEvent("ADDON_LOADED", this.name);
+		INFO(format("Addon %s was successfully loaded", this.name));
+
+		if (!this.onLoad instanceof Function) return;
+		this.onLoad();
+	}
+}
