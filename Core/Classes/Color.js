46c0f423d9b0542d4535faf73df259ddeead4ea8
RDW <duckwhale@mail.ru>
2021-05-24 22:52:19 +0200

    Core: Updated color presets to include magenta and purple
    They are useful as transparent background color in sprites and textures.

--- Core/Classes/Color.js
+++ Core/Classes/Color.js
@@ -5,6 +5,8 @@ class Color {
 	static BLACK = { red: 0, green: 0, blue: 0, alpha: 255 };
 	static WHITE = { red: 255, green: 255, blue: 255, alpha: 255 };
 	static GREY = { red: 200, green: 200, blue: 200, alpha: 255 };
+	static MAGENTA = { red: 255, green: 0, blue: 255, alpha: 255 };
+	static PURPLE = { red: 200, green: 0, blue: 200, alpha: 255 };
 
 	constructor(red, green, blue, alpha = 255) {
 		this.red = red;
