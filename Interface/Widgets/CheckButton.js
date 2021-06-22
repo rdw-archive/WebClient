635269f446e067c344ed330273c363cf3df6c811
RDW <duckwhale@mail.ru>
2021-06-22 22:32:38 +0200

    UI: Fixed label alignment of the CheckButton widget

--- Interface/Widgets/CheckButton.js
+++ Interface/Widgets/CheckButton.js
@@ -2,22 +2,26 @@ class CheckButton extends Widget {
 	constructor(widgetName, parentFrame, template) {
 		super(widgetName, parentFrame, template);
 		const widgetContainer = new Frame(widgetName + "Container", parentFrame, "CheckButtonContainer");
-		const label = new Label(widgetName + "Label", widgetContainer);
-		label.createFontString(widgetName + "LabelText", "MEDIUM", "GameFontSmall");
-		// todo ugly, use API?
-		label._obj.htmlFor = widgetName;
+
 		this.container = widgetContainer;
-		this.label = label;
 		// 	widgetContainer:EnableMouse(true)
 
 		// TODO DRY
 		this._obj = document.createElement("input");
 		this._obj.type = "checkbox";
 		this._obj.id = widgetName;
+		this._obj.className = "CheckButton";
 		if (template) this._obj.className = template;
 
+		this.setParent(widgetContainer);
+
+		const label = new Label(widgetName + "Label", widgetContainer);
+		label.createFontString(widgetName + "LabelText", "MEDIUM", "GameFontSmall");
+		this.label = label;
+		label._obj.htmlFor = widgetName;
+		// todo ugly, use API?
+
 		this.setName(widgetName);
-		this.setParent(parentFrame);
 	}
 	setText(text) {
 		this.label.setText(text);
