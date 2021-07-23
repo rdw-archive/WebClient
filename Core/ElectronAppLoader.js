d989b764a08e243902e4c47891471273aac81735
RDW <duckwhale@mail.ru>
2021-07-23 08:43:00 +0200

    Core: Added ElectronAppLoader class to aid with loading settings
    The name and structure likely needs a second look, but with the tests in place this can be tackled individually.

---
+++ Core/ElectronAppLoader.js
@@ -0,0 +1,49 @@
+const { readFileSync, existsSync, writeFileSync } = require("fs");
+const Joi = require("joi");
+
+const schema = Joi.object({
+    enableDevTools: Joi.boolean().optional(),
+    devToolsDockingMode: Joi.string().optional().valid("right", "bottom", "undocked", "detach")
+});
+
+const DEFAULT_MAIN_THREAD_SETTINGS_PATH = "Config/electron-main.js";
+// ElectronAppLauncher
+class ElectronAppLoader {
+    constructor() {
+        this.settings = {};
+        this.settingsPath = DEFAULT_MAIN_THREAD_SETTINGS_PATH;
+    }
+    setSettingsPath(filePath) {
+        this.settingsPath = filePath;
+    }
+    loadSettingsFromDisk() {
+        if (!existsSync(this.settingsPath))
+            throw new Error("Failed to load settings from file " + this.settingsPath + " (File not found)");
+
+        let settings = readFileSync(this.settingsPath);
+
+        try {
+            settings = JSON.parse(settings);
+            this.settings = settings;
+        } catch (SyntaxError) {
+            throw new Error("Failed to load settings from file " + this.settingsPath + " (Invalid JSON)");
+        }
+        const validationResult = schema.validate(settings);
+        if (validationResult.error)
+            throw new Error("Failed to load settings from file " + this.settingsPath + " (Settings are invalid)");
+    }
+    saveSettingsToDisk() {
+        const validatedSettings = {};
+
+        for (const fieldName in this.settings) {
+            const value = this.settings[fieldName];
+            const validationResult = schema.validate({ [fieldName]: value });
+            if (!validationResult.error) validatedSettings[fieldName] = value;
+        }
+
+        const serializedSettingsString = JSON.stringify(validatedSettings);
+        writeFileSync(this.settingsPath, serializedSettingsString);
+    }
+}
+
+module.exports = ElectronAppLoader;
