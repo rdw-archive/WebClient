ab9400e6bdf485d233a320967dc2998ec9cfdedf
RDW <duckwhale@mail.ru>
2021-07-25 12:58:31 +0200

    Tests: Added a basic test suite for the Settings API

---
+++ Tests/API/C_Settings/validate.js
@@ -0,0 +1,20 @@
+describe("The C_Settings API", function () {
+	it("should succeed validating arbitrary settings when the default settings are passed", function () {
+		const defaultSettings = C_FileSystem.readJSON(
+			C_Settings.DEFAULT_SETTINGS_FILE_PATH
+		);
+		assertTrue(C_Settings.validate(defaultSettings));
+	});
+	it("should fail validating arbitrary settings when nothing is passed", function () {
+		assertFalse(C_Settings.validate(undefined));
+		assertFalse(C_Settings.validate(null));
+	});
+	it("should fail validating arbitrary settings when an empty object is passed", function () {
+		assertFalse(C_Settings.validate({}));
+	});
+	it("should fail validating arbitrary settings when objects of the wrong type are passed", function () {
+		assertFalse(C_Settings.validate("NotASettingsObject"));
+		assertFalse(C_Settings.validate(42));
+		assertFalse(C_Settings.validate(function () {}));
+	});
+});
