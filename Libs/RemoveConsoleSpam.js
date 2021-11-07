2e3c46f114a7257d6d0b5b059942b6ff6af97d09
RDW <duckwhale@mail.ru>
2021-11-07 09:38:11 +0100

    Cleanup: Unblock third-party messages
    They're still annoying, but maybe seeing them all the time will finally motivate me to actually solve the problem.

--- Libs/RemoveConsoleSpam.js
+++
@@ -1,2 +0,0 @@
-window.__REACT_DEVTOOLS_GLOBAL_HOOK__ = { isDisabled: true };
-window.ELECTRON_DISABLE_SECURITY_WARNINGS = true; // Even with a safe CSP, the spam doesn't stop. Who designs these features? Please punch them. Hard.
