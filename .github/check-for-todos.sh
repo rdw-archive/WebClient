675f1f64ac436d7598aac73b33be8ae06a9ac47c
RDW <duckwhale@mail.ru>
2021-11-12 01:30:44 +0100

    Repo: Add a shell script to detect when TODOs etc are introduced
    It's not winning any awards for elegance, but for now it'll do.

---
+++ .github/check-for-todos.sh
@@ -0,0 +1,61 @@
+set +e
+
+echo "Checking for temporary comments..."
+
+echo ""
+
+NUMBER_OF_COMMITTED_TODOS=$(git diff | grep -o 'TODO' | wc -l)
+NUMBER_OF_COMMITTED_TBDS=$(git diff | grep -o 'TBD' | wc -l)
+NUMBER_OF_COMMITTED_WIPS=$(git diff | grep -o 'WIP' | wc -l)
+NUMBER_OF_COMMITTED_FIXMES=$(git diff | grep -o 'FIXME' | wc -l)
+NUMBER_OF_COMMITTED_HACKS=$(git diff | grep -o 'HACK' | wc -l)
+
+echo "Committed TODOs: $NUMBER_OF_COMMITTED_TODOS"
+echo "Committed TBDs: $NUMBER_OF_COMMITTED_TBDS"
+echo "Committed WIPs: $NUMBER_OF_COMMITTED_WIPS"
+echo "Committed FIXMEs: $NUMBER_OF_COMMITTED_FIXMES"
+echo "Committed HACKs: $NUMBER_OF_COMMITTED_HACKS"
+
+echo ""
+
+if [ $NUMBER_OF_COMMITTED_TODOS -ne 0 ]; then
+	echo "Failed code check: TODOs are present and should be removed"
+	echo ""
+	echo "If the comments indicate an actual problem, feature, or improvement,"
+	echo "they ought to be converted to a proper issue instead!"
+	exit 1
+fi
+
+if [ $NUMBER_OF_COMMITTED_TBDS -ne 0 ]; then
+	echo "Failed code check: TBDs are present and should be removed"
+	echo ""
+	echo "If the comments indicate an actual problem, feature, or improvement,"
+	echo "they ought to be converted to a proper issue instead!"
+	exit 2
+fi
+
+if [ $NUMBER_OF_COMMITTED_WIPS -ne 0 ]; then
+	echo "Failed code check: WIPs are present and should be removed"
+	echo ""
+	echo "If the comments indicate an actual problem, feature, or improvement,"
+	echo "they ought to be converted to a proper issue instead!"
+	exit 3
+fi
+
+if [ $NUMBER_OF_COMMITTED_FIXMES -ne 0 ]; then
+	echo "Failed code check: FIXMEs are present and should be removed"
+	echo ""
+	echo "If the comments indicate an actual problem, feature, or improvement,"
+	echo "they ought to be converted to a proper issue instead!"
+	exit 4
+fi
+
+if [ $NUMBER_OF_COMMITTED_HACKS -ne 0 ]; then
+	echo "Failed code check: HACKs are present and should be removed"
+	echo ""
+	echo "If the comments indicate an actual problem, feature, or improvement,"
+	echo "they ought to be converted to a proper issue instead!"
+	exit 5
+fi
+
+exit 0
