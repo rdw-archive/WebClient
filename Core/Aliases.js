3e6e79742abba6d8f026bcd949e5b0691a51699d
RDW <Duckwhale@mail.ru>
2021-11-17 14:58:04 +0100

    Core: Expose global UUID alias for the uuid API

--- Core/Aliases.js
+++ Core/Aliases.js
@@ -9,6 +9,7 @@ const BABYLON = require("babylonjs/babylon.max"); // C_WebGL & C_Sound
 const BITMAP = require("bmp-js"); // C_Bitmap
 const MESSAGEPACK = require("@msgpack/msgpack"); // C_Message
 const JOI = require("joi"); // C_Validation
+const UUID = require("uuid"); // UniqueID builtin
 
 function dump(...data) {
 	console.log(data);
