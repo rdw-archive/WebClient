bba1685e16c76f79e88b0fa9355c0c5a2e1a2bee
RDW <Duckwhale@mail.ru>
2021-11-19 17:01:25 +0100

    UI: Add more options to the WebAudio configuration interface

--- Interface/Frames/SystemOptionsFrame.js
+++ Interface/Frames/SystemOptionsFrame.js
@@ -12,8 +12,10 @@ const SystemOptions = {
 		this.frame.hide();
 	},
 	createWidgets() {
-		// Create WebAudio settings panel
-		let soundOptionsPanel = new Frame(
+		this.createWebAudioOptions();
+	},
+	createWebAudioOptions() {
+		const soundOptionsPanel = new Frame(
 			"SystemOptionsFrame_SoundOptionsPanel",
 			this.frame._obj.content.rightPane,
 			"OptionsFrameCategoryPanel"
@@ -31,118 +33,190 @@ const SystemOptions = {
 		);
 		soundOptionsPanel.headerText.setText("WebAudio Configuration");
 
-		// Create general settings group
-		let generalSettingsGroup = new OptionsGroup(
+		this.soundOptionsPanel = soundOptionsPanel;
+
+		this.createGeneralSettingsGroup();
+		this.createChannelSettingsGroup();
+		this.createVolumeSettingsGroup();
+
+		this.frame.addCategoryPanel("Sound", soundOptionsPanel);
+	},
+	createGeneralSettingsGroup() {
+		const generalSettingsGroup = new OptionsGroup(
 			"SystemOptionsFrame_SoundOptions_GeneralSettingsGroup",
-			soundOptionsPanel,
+			this.soundOptionsPanel,
 			"OptionsGroup"
 		);
 		generalSettingsGroup.setCaption(L["General"]);
 		generalSettingsGroup.setCaptionStyle("CaptionFontSmall");
-		// Create global sound toggle checkbox
-		let checkbox = new CheckButton(
-			"SystemOptionsFrame_SoundOptions_GeneralSettingsGroup_GlobalSoundToggle",
-			generalSettingsGroup.fieldSet
-		);
-		checkbox.setScript("OnClick", () => {
-			DEBUG("Functionality NYI");
-		});
-		checkbox.setText(L["Enable Sound"]);
+		this.generalSettingsGroup = generalSettingsGroup;
 
-		// Create audio channel settings group
-		let channelSettingsGroup = new OptionsGroup(
+		this.createGlobalSoundToggle();
+		this.createHighQualityStereoToggle();
+		this.createBackgroundVolumeToggle();
+		this.createBackgroundVolumeSlider();
+	},
+	createGlobalSoundToggle() {
+		const enableSoundCheckbox = new CheckButton(
+			"SystemOptionsFrame_SoundOptions_GeneralSettingsGroup_GlobalSoundToggle",
+			this.generalSettingsGroup.fieldSet
+		);
+		enableSoundCheckbox.setScript("OnClick", () => {
+			if (!enableSoundCheckbox.isChecked()) C_WebAudio.disableSound();
+			else C_WebAudio.enableSound();
+		});
+		enableSoundCheckbox.setText(L["Enable Sound"]);
+		enableSoundCheckbox.setChecked(C_Settings.getValue("enableSound"));
+	},
+	createHighQualityStereoToggle() {
+		const useHighQualityStereoCheckbox = new CheckButton(
+			"SystemOptionsFrame_SoundOptions_GeneralSettingsGroup_HighQualityStereoToggle",
+			this.generalSettingsGroup.fieldSet
+		);
+		useHighQualityStereoCheckbox.setScript("OnClick", () => {
+			if (!useHighQualityStereoCheckbox.isChecked()) C_WebAudio.disableHighQualityStereo();
+			else C_WebAudio.enableHighQualityStereo();
+		});
+		useHighQualityStereoCheckbox.setText(L["Use High-Quality Stereo"]);
+		useHighQualityStereoCheckbox.setChecked(C_Settings.getValue("useHighQualityStereo"));
+	},
+	createBackgroundVolumeToggle() {
+		const soundInBackgroundCheckbox = new CheckButton(
+			"SystemOptionsFrame_SoundOptions_GeneralSettingsGroup_BackgroundFadeToggle",
+			this.generalSettingsGroup.fieldSet
+		);
+		soundInBackgroundCheckbox.setScript("OnClick", () => {
+			if (!soundInBackgroundCheckbox.isChecked()) C_WebAudio.disableBackgroundFade();
+			else C_WebAudio.enableBackgroundFade();
+		});
+		soundInBackgroundCheckbox.setText(L["Lower Volume in Background"]);
+		soundInBackgroundCheckbox.setChecked(C_Settings.getValue("fadeSoundInBackground"));
+	},
+	createBackgroundVolumeSlider() {
+		const backgroundVolumeSlider = new Slider(
+			"SystemOptionsFrame_SoundOptions_VolumeSettingsGroup_BackgroundVolumeSlider",
+			this.generalSettingsGroup.fieldSet
+		);
+		backgroundVolumeSlider.setLabelText(L["Background Volume"]);
+		backgroundVolumeSlider.setScript("OnInput", () => {
+			C_WebAudio.setBackgroundVolume(backgroundVolumeSlider.getValue() / 100);
+		});
+		backgroundVolumeSlider.setValue(C_Settings.getValue("backgroundAudioVolume") * 100);
+	},
+	createChannelSettingsGroup() {
+		const channelSettingsGroup = new OptionsGroup(
 			"SystemOptionsFrame_SoundOptions_ChannelSettingsGroup",
-			soundOptionsPanel,
+			this.soundOptionsPanel,
 			"OptionsGroup"
 		);
 		channelSettingsGroup.setCaption(L["Audio Channels"]);
 		channelSettingsGroup.setCaptionStyle("CaptionFontSmall");
-		// Create checkboxes
 
-		let backgroundMusicToggle = new CheckButton(
-			"SystemOptionsFrame_SoundOptions_ChannelSettingsGroup_BackgroundbackgroundMusicToggle",
-			channelSettingsGroup.fieldSet
+		this.channelSettingsGroup = channelSettingsGroup;
+
+		this.createMusicToggle();
+		this.createEffectSoundToggle();
+		this.createAmbientSoundToggle();
+	},
+	createMusicToggle() {
+		const musicToggle = new CheckButton(
+			"SystemOptionsFrame_SoundOptions_ChannelSettingsGroup_MusicToggle",
+			this.channelSettingsGroup.fieldSet
 		);
-		backgroundMusicToggle.setScript("OnClick", () => {
-			DEBUG("Functionality NYI");
+		musicToggle.setScript("OnClick", () => {
+			if (!musicToggle.isChecked()) C_WebAudio.disableMusicTrack();
+			else C_WebAudio.enableMusicTrack();
 		});
-		backgroundMusicToggle.setText(L["Music"]);
-
-		let soundEffectToggle = new CheckButton(
+		musicToggle.setText(L["Music"]);
+		musicToggle.setChecked(C_Settings.getValue("enableMusic"));
+	},
+	createEffectSoundToggle() {
+		const soundEffectToggle = new CheckButton(
 			"SystemOptionsFrame_SoundOptions_ChannelSettingsGroup_SoundEffectsToggle",
-			channelSettingsGroup.fieldSet
+			this.channelSettingsGroup.fieldSet
 		);
 		soundEffectToggle.setScript("OnClick", () => {
-			DEBUG("Functionality NYI");
+			if (!soundEffectToggle.isChecked()) C_WebAudio.disableEffectsTrack();
+			else C_WebAudio.enableEffectsTrack();
 		});
 		soundEffectToggle.setText(L["Sound Effects"]);
-
-		let ambientSoundToggle = new CheckButton(
+		soundEffectToggle.setChecked(C_Settings.getValue("enableSFX"));
+	},
+	createAmbientSoundToggle() {
+		const ambientSoundToggle = new CheckButton(
 			"SystemOptionsFrame_SoundOptions_ChannelSettingsGroup_AmbientSoundsToggle",
-			channelSettingsGroup.fieldSet
+			this.channelSettingsGroup.fieldSet
 		);
 		ambientSoundToggle.setScript("OnClick", () => {
-			DEBUG("Functionality NYI");
+			if (!ambientSoundToggle.isChecked()) C_WebAudio.disableAmbienceTrack();
+			else C_WebAudio.enableAmbienceTrack();
 		});
 		ambientSoundToggle.setText(L["Ambient Sounds"]);
-
-		let soundInBackgroundToggle = new CheckButton(
-			"SystemOptionsFrame_SoundOptions_ChannelSettingsGroup_SoundInBackgroundToggle",
-			channelSettingsGroup.fieldSet
-		);
-		soundInBackgroundToggle.setScript("OnClick", () => {
-			DEBUG("Functionality NYI");
-		});
-		soundInBackgroundToggle.setText(L["Sound in Background"]);
-
-		// Create volume settings group
-		let volumeSettingsGroup = new OptionsGroup(
+		ambientSoundToggle.setChecked(C_Settings.getValue("enableAmbientSounds"));
+	},
+	createVolumeSettingsGroup() {
+		const volumeSettingsGroup = new OptionsGroup(
 			"SystemOptionsFrame_SoundOptions_VolumeSettingsGroup",
-			soundOptionsPanel,
+			this.soundOptionsPanel,
 			"OptionsGroup"
 		);
 		volumeSettingsGroup.setCaption(L["Volume Settings"]);
 		volumeSettingsGroup.setCaptionStyle("CaptionFontSmall");
-		// Create sliders
-		let masterVolumeSlider = new Slider(
-			"SystemOptionsFrame_SoundOptions_VolumeSettingsGroup_MasterVolumeSlider",
-			volumeSettingsGroup.fieldSet
-		);
-		masterVolumeSlider.setLabelText(L["Master Volume"]);
-		masterVolumeSlider.setScript("OnInput", () => {
-			DEBUG("Functionality NYI");
-		});
+		this.volumeSettingsGroup = volumeSettingsGroup;
 
-		let effectsVolumeSlider = new Slider(
-			"SystemOptionsFrame_SoundOptions_VolumeSettingsGroup_EffectsVolumeSlider",
-			volumeSettingsGroup.fieldSet
+		this.createVolumeSettingsSliders();
+	},
+	createVolumeSettingsSliders() {
+		this.createGlobalVolumeSlider();
+		this.createMusicVolumeSlider();
+		this.createEffectsVolumeSlider();
+		this.createAmbienceVolumeSlider();
+	},
+	createGlobalVolumeSlider() {
+		const globalVolumeSlider = new Slider(
+			"SystemOptionsFrame_SoundOptions_VolumeSettingsGroup_GlobalVolumeSlider",
+			this.volumeSettingsGroup.fieldSet
 		);
-		effectsVolumeSlider.setLabelText(L["Sound Effects"]);
-		effectsVolumeSlider.setScript("OnInput", () => {
-			DEBUG("Functionality NYI");
+		globalVolumeSlider.setLabelText(L["Global Volume"]);
+		globalVolumeSlider.setScript("OnInput", () => {
+			C_WebAudio.setGlobalVolume(globalVolumeSlider.getValue() / 100);
 		});
-
-		let musicVolumeSlider = new Slider(
+		globalVolumeSlider.setValue(C_Settings.getValue("globalVolume") * 100);
+	},
+	createMusicVolumeSlider() {
+		const musicVolumeSlider = new Slider(
 			"SystemOptionsFrame_SoundOptions_VolumeSettingsGroup_MusicVolumeSlider",
-			volumeSettingsGroup.fieldSet
+			this.volumeSettingsGroup.fieldSet
 		);
+
 		musicVolumeSlider.setLabelText(L["Music"]);
 		musicVolumeSlider.setScript("OnInput", () => {
-			DEBUG("Functionality NYI");
+			C_WebAudio.setMusicVolume(musicVolumeSlider.getValue() / 100);
 		});
+		musicVolumeSlider.setValue(C_Settings.getValue("musicVolume") * 100);
+	},
+	createEffectsVolumeSlider() {
+		const effectsVolumeSlider = new Slider(
+			"SystemOptionsFrame_SoundOptions_VolumeSettingsGroup_EffectsVolumeSlider",
+			this.volumeSettingsGroup.fieldSet
+		);
 
-		let ambienceVolumeSlider = new Slider(
+		effectsVolumeSlider.setLabelText(L["Sound Effects"]);
+		effectsVolumeSlider.setScript("OnInput", () => {
+			C_WebAudio.setEffectsVolume(effectsVolumeSlider.getValue() / 100);
+		});
+		effectsVolumeSlider.setValue(C_Settings.getValue("sfxVolume") * 100);
+	},
+	createAmbienceVolumeSlider() {
+		const ambienceVolumeSlider = new Slider(
 			"SystemOptionsFrame_SoundOptions_VolumeSettingsGroup_AmbienceVolumeSlider",
-			volumeSettingsGroup.fieldSet
+			this.volumeSettingsGroup.fieldSet
 		);
 		ambienceVolumeSlider.setLabelText(L["Ambience"]);
 		ambienceVolumeSlider.setScript("OnInput", () => {
-			DEBUG("Functionality NYI");
+			C_WebAudio.setAmbienceVolume(ambienceVolumeSlider.getValue() / 100);
 		});
-
-		this.frame.addCategoryPanel("Sound", soundOptionsPanel);
-
-		this.soundOptionsPanel = soundOptionsPanel;
+		ambienceVolumeSlider.setValue(C_Settings.getValue("ambienceVolume") * 100);
 	},
 };
 
