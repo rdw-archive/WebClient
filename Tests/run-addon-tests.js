8239e8171b99258a1ab68bb9c559e29713cf1adb
RDW <Duckwhale@mail.ru>
2021-12-11 04:15:17 +0100

    Cleanup: Remove the obsolete addon test suite handling

--- Tests/run-addon-tests.js
+++
@@ -1,6 +0,0 @@
-Channels[Enum.LOG_LEVEL_DEBUG] = null;
-Channels[Enum.LOG_LEVEL_INFO] = null;
-Channels[Enum.LOG_LEVEL_WARNING] = null;
-
-// This executes whatever tests have been registered by addons (on load, via C_Testing.registerTestSuite)
-C_Testing.loadAddonTests();
