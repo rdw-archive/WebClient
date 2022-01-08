4210bb269e63171be77888cf00792f4c83806299
RDW <Duckwhale@mail.ru>
2022-01-08 18:01:32 +0100

    Update Vector3D.js

--- Core/Classes/Vector3D.js
+++ Core/Classes/Vector3D.js
@@ -5,4 +5,13 @@ class Vector3D {
 		this.y = y;
 		this.z = z;
 	}
+	static normalizeInPlace(vector3D) {
+		const vector = new BABYLON.Vector3(vector3D.x, vector3D.y, vector3D.z);
+		vector.normalize();
+
+		// TBD It was a NO-OP, that doesn't seem right?
+		// vector3D.x = vector.x
+		// vector3D.y = vector.y
+		// vector3D.z = vector.z
+	}
 }
