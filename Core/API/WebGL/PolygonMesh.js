9465321cd3c3c930aad184719f605b2fd33124d9
RDW <Duckwhale@mail.ru>
2022-01-13 14:54:13 +0100

    Add polygon mesh wrapper class

---
+++ Core/API/WebGL/PolygonMesh.js
@@ -0,0 +1,26 @@
+class PolygonMesh {
+	constructor(meshBlueprint) {
+		this.textureFilePath = null;
+		this.lightmapTextureFilePath = null;
+		this.ambientTextureFilePath = null;
+
+		this.vertices = [];
+		this.connections = [];
+		this.vertexColors = [];
+		this.textureCoordinates = [];
+		this.flatNormals = [];
+		this.smoothNormals = [];
+		this.lightmapUVs = []; // Also used for the ambient occlusion map (for now)
+	}
+	setLightmapTexturePath(textureFilePath) {
+		this.lightmapTextureFilePath = textureFilePath;
+	}
+	setAmbientOcclusionTexturePath(textureFilePath) {
+		this.ambientTextureFilePath = textureFilePath;
+	}
+	setDiffuseTexturePath(textureFilePath) {
+		this.textureFilePath = textureFilePath;
+	}
+	render() {	}
+	destroy() {}
+}
