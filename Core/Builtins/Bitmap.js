ad3ee9505502cf7ef65973046c5307a9b4958625
RDW <Duckwhale@mail.ru>
2022-02-21 20:09:56 +0100

    Refactor: Streamline the Bitmap builtin

--- Core/Builtins/Bitmap.js
+++ Core/Builtins/Bitmap.js
@@ -1,12 +1,16 @@
-// Danger: Here be dragons (In serious need of refactoring; it's essentially prototype-quality code)
-
-const NUM_BITS_PER_BYTE = 8; // Err, really? What was I thinking?
-
 class Bitmap {
-	constructor(pixelData = [], width = 0, height = 0) {
+	static MAGIC_HEADER = "BM";
+	constructor(pixelData, width = 0, height = 0, pixelFormat = Enum.PIXEL_FORMAT_RGBA) {
+		if (!pixelData) pixelData = new Uint8ClampedArray(width * height * 4);
+
 		this.pixelData = pixelData;
 		this.width = width;
 		this.height = height;
+
+		this.pixelFormat = pixelFormat;
+		this.bytesPerPixel = 4; // We always store the RGBA values for each pixel
+
+		this.transparencyColor = null;
 	}
 	flipHorizontally(pixelData = this.pixelData, imageWidth = this.width, imageHeight = this.height) {
 		let flippedPixelData = [];
@@ -31,27 +35,88 @@ class Bitmap {
 			}
 		}
 
-		this.pixelData = flippedPixelData;
+		this.pixelData = new Uint8ClampedArray(flippedPixelData);
 	}
-	finalizePixelData() {
-		this.pixelData = Uint8ClampedArray.from(this.pixelData); // TODO: Is this actually needed?
-		// Keeping this around after the atlas image is created (below) wastes some space, but allows accessing the individual sprites more easily. I guess it's useful for debugging only?)
-		// TODO Remove after atlas is created?
-	}
-	// TODO: What about the frame ID? Do we need it still?
-	// frameID: frameIndex, // NOT redundant; we need a way to identify frames in the bitmap later (the frame index is refered in the sprite's ACT data, but the bin packer sorts them and reassigns the index while creating the atlas...)
-	addPixel(red, green, blue, alpha = 1) {
-		this.pixelData.push(red);
-		this.pixelData.push(green);
-		this.pixelData.push(blue);
-		this.pixelData.push(alpha);
-	}
-}
+	// DEPRECATED: This can't currently be removed, but it really should be refactored at some point
+	setTransparencyColor(transparencyColor, sourcePixelFormat = Enum.PIXEL_FORMAT_RGBA) {
+		C_Profiling.startTimer("Replacing transparent diffuse texture pixels");
 
+		const pixelData = this.pixelData;
+		if (sourcePixelFormat === Enum.PIXEL_FORMAT_ABGR) {
+			for (let pixelIndex = 0; pixelIndex < this.pixelData.length; pixelIndex += 1) {
+				let pixelOffset = pixelIndex * 4;
 
-		}
+				let red = pixelData[pixelOffset + 3];
+				let green = pixelData[pixelOffset + 2];
+				let blue = pixelData[pixelOffset + 1];
 
-
+				if (transparencyColor.red === red && transparencyColor.green === green && transparencyColor.blue === blue) {
+					pixelData[pixelOffset + 0] = 0; // alpha
+				}
 			}
 		}
 
+		if (sourcePixelFormat === Enum.PIXEL_FORMAT_RGBA) {
+			for (let pixelIndex = 0; pixelIndex < this.pixelData.length; pixelIndex += 1) {
+				let pixelOffset = pixelIndex * 4;
+
+				let red = pixelData[pixelOffset + 0];
+				let green = pixelData[pixelOffset + 1];
+				let blue = pixelData[pixelOffset + 2];
+
+				if (transparencyColor.red === red && transparencyColor.green === green && transparencyColor.blue === blue) {
+					pixelData[pixelOffset + 3] = 0;
+				}
+			}
+		}
+
+		C_Profiling.endTimer("Replacing transparent diffuse texture pixels");
+
+		this.transparencyColor = transparencyColor;
+	}
+	toRGBA(sourcePixelFormat = Enum.PIXEL_FORMAT_ABGR) {
+		const pixelData = this.pixelData;
+		const pixelBuffer = new Uint8ClampedArray(pixelData.length);
+
+		if (sourcePixelFormat === Enum.PIXEL_FORMAT_ABGR) {
+			for (let pixelID = 0; pixelID < pixelData.length / 4; pixelID++) {
+				const alpha = pixelData[pixelID * 4 + 0];
+				const blue = pixelData[pixelID * 4 + 1];
+				const green = pixelData[pixelID * 4 + 2];
+				const red = pixelData[pixelID * 4 + 3];
+
+				// Swap them to generate RGBA
+				pixelBuffer[pixelID * 4 + 0] = red;
+				pixelBuffer[pixelID * 4 + 1] = green;
+				pixelBuffer[pixelID * 4 + 2] = blue;
+				pixelBuffer[pixelID * 4 + 3] = alpha;
+			}
+		}
+		this.pixelFormat = Enum.PIXEL_FORMAT_RGBA;
+		this.pixelData = pixelBuffer;
+	}
+	getPixelFormat() {
+		return this.pixelFormat;
+	}
+	getBufferSize() {
+		return this.pixelData.length;
+	}
+	computeChecksum() {
+		return CRC32.fromArrayBuffer(this.pixelData);
+	}
+	getWidth() {
+		return this.width;
+	}
+	getHeight() {
+		return this.height;
+	}
+	hasAlpha() {
+		return this.transparencyColor instanceof Color;
+	}
+	getTransparencyColor() {
+		return this.transparencyColor;
+	}
+	toArrayBuffer() {
+		return new Uint8ClampedArray(this.pixelData);
+	}
+}
