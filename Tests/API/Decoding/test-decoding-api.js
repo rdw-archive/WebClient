200fcb645f4ab65f3df56453a26672d140886b9f
RDW <Duckwhale@mail.ru>
2022-02-26 06:24:13 +0100

    Tests: Add a basic test for expected decoding errors

--- Tests/API/Decoding/test-decoding-api.js
+++ Tests/API/Decoding/test-decoding-api.js
@@ -6,3 +6,29 @@ describe("canDecodeFile", () => {
 		assertTrue(C_Decoding.canDecodeFile("test.png"));
 	});
 });
+
+describe("decodeFile", () => {
+	it("should raise an error if the file type is not supported", () => {
+		const fileName = new UniqueID().toString() + ".asdf";
+
+		// Setup
+		assertFalse(C_FileSystem.fileExists(fileName));
+		C_FileSystem.writeJSON(fileName, {});
+		assertTrue(C_FileSystem.fileExists(fileName));
+
+		const expectedError = new Error("Cannot decode " + fileName + " (file type ASDF is not supported)");
+		assertThrows(() => C_Decoding.decodeFile(fileName), expectedError);
+
+		// Teardown
+		C_FileSystem.removeFile(fileName);
+		assertFalse(C_FileSystem.fileExists(fileName));
+	});
+
+	it("should raise an error if the given path is invalid", () => {
+		const fileName = new UniqueID().toString() + ".asdf";
+		assertFalse(C_FileSystem.fileExists(fileName)); // Just making sure...
+
+		const expectedError = new Error("Cannot decode " + fileName + " (No such file exists)");
+		assertThrows(() => C_Decoding.decodeFile(fileName), expectedError);
+	});
+});
