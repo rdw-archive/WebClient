829b99dc2c84bfdbb0027e76d3b549c5f33bb2c9
RDW <Duckwhale@mail.ru>
2022-03-03 06:41:58 +0100

    API: Add some experimental builtins to the WebGL API
    Needs a better design, but until I've thought of one there should be something in between BJS and the engine code to avoid having to untangle a large mess of spaghetti later.

---
+++ Core/API/WebGL/GeometryBlueprint.js
@@ -0,0 +1,38 @@
+class GeometryBlueprint {
+	constructor() {
+		this.vertices = [];
+		this.connections = [];
+		this.vertexColors = [];
+		this.normalVectors = [];
+		this.diffuseTextureCoordinates = [];
+		this.lightmapTextureCoordinates = []; // Also used for the ambient occlusion map (for now)
+
+		this.lightmapTextureImage = null;
+		this.ambientOcclusionTextureImage = null;
+		this.diffuseTextureImage = null;
+
+		this.flipTextureImages = true;
+	}
+	getPackedSize() {
+		const BYTES_PER_INTEGER = 4;
+		const BYTES_PER_FLOAT = 4;
+
+		const positionsSize = this.vertices.length * BYTES_PER_FLOAT;
+		const indicesSize = this.connections.length * BYTES_PER_INTEGER;
+		const vertexColorsSize = this.vertexColors.length * BYTES_PER_FLOAT;
+		const flatNormalsSize = this.normalVectors.length * BYTES_PER_FLOAT;
+		const lightmapTexCoordsSize = this.lightmapTextureCoordinates.length * BYTES_PER_FLOAT;
+		const diffuseTexCoordsSize = this.diffuseTextureCoordinates.length * BYTES_PER_FLOAT;
+
+		const fieldCountsSize = BYTES_PER_INTEGER * 6; // The relevant arrays (all of the above)
+		return (
+			fieldCountsSize +
+			positionsSize +
+			indicesSize +
+			vertexColorsSize +
+			flatNormalsSize +
+			lightmapTexCoordsSize +
+			diffuseTexCoordsSize
+		);
+	}
+}
---
+++ Core/API/WebGL/PolygonMesh.js
@@ -0,0 +1,40 @@
+class PolygonMesh {
+	constructor(name = new UniqueID().toString(), geometryBlueprint) {
+		this.displayName = name;
+		this.diffuseTexture = null;
+		this.lightmapTexture = null;
+		this.ambientOcclusionTexture = null;
+
+		this.sceneObject = null;
+
+		this.geometryBlueprint = geometryBlueprint;
+	}
+	setDiffuseTexture(diffuseTextureImage) {
+		this.diffuseTexture = diffuseTextureImage;
+	}
+	setLightmapTexture(lightmapTextureImage) {
+		this.lightmapTexture = lightmapTextureImage;
+	}
+	setAmbientOcclusionTexture(ambientOcclusionTextureImage) {
+		this.ambientOcclusionTexture = ambientOcclusionTextureImage;
+	}
+	getBlueprint() {
+		return this.geometryBlueprint;
+	}
+	setDisplayName(name) {
+		this.name = name;
+	}
+	getDisplayName() {
+		return this.displayName;
+	}
+	render() {
+		const sceneObject = C_WebGL.createMesh(this.displayName, this.geometryBlueprint);
+		C_Rendering.addMesh(this.displayName, sceneObject);
+
+		this.sceneObject = sceneObject;
+	}
+	destroy() {
+		C_Rendering.removeMesh(this.sceneObject);
+		this.sceneObject = null;
+	}
+}
